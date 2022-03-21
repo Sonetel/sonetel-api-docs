c975ba1e788b050f140d7a3fd0eeb0be27478cd2
harshpatel1242 <harshpatel1242@gmail.com>
2022-03-21 16:31:01 +0530

    Update Subscribe_Phone_Number.php
    Updated Structure(removed the user interactive flow)

--- sample-scripts/buy-number/Subscribe_Phone_Number.php
+++ sample-scripts/buy-number/Subscribe_Phone_Number.php
@@ -1,140 +1,36 @@
 <?php
 
-// #base urls
-$base_url = "https://public-api.sonetel.com/";
-$url_access_token = "https://api.sonetel.com/";
-// #inputs
-$username = readline('Enter your username: ');
-$password = readline('Enter your password: ');
-$country = readline("Enter the name of the nation whose number you wish to purchase:");
+// API Access token
+$access_token = "ENTER_ACCESS_TOKEN";
 
-// #access token
-$curl_access_token = curl_init();
+// Phone numumber to purchase
+$phnum = "ENTER_PHONE_NUMBER";
 
-curl_setopt_array($curl_access_token, array(
-  CURLOPT_URL => $url_access_token.'SonetelAuth/oauth/token',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => array('grant_type' => 'password','username' => strval($username),'password' => strval($password),'refresh' => 'yes'),
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: Basic c29uZXRlbC13ZWI6c29uZXRlbC13ZWI='
-  ),
-));
+// Sonetel account ID
+$acc_id =  "ENTER_ACCOUNT_ID";
 
-$response_access_token = curl_exec($curl_access_token);
-curl_close($curl_access_token);
-// #json decode
-$json_response_access_token = json_decode($response_access_token);
-$access_token = $json_response_access_token->{'access_token'};
-
-// #avail phone num
-$conc_avail_phnum = $base_url."numberstocksummary/".$country."/availablephonenumber";
-$curl_avail_phnum = curl_init();
-
-curl_setopt_array($curl_avail_phnum, array(
-  CURLOPT_URL => $conc_avail_phnum,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json',
-    'Authorization: Bearer '.trim($access_token),
-  ),
-));
-
-// #response
-$response_avail_phnum = curl_exec($curl_avail_phnum);
-curl_close($curl_avail_phnum);
-
-// #json
-$json_response_avail_phnum = json_decode($response_avail_phnum);
-
-// #phone num
-$phnum = $json_response_avail_phnum->{'response'}[0]->{'phnum'};
-
-// #charge of particular numb
-$num_fee = $json_response_avail_phnum->{'response'}[0]->{'recurring_fee'};
-
-// #account info
-$curl_acc_info = curl_init();
-
-curl_setopt_array($curl_acc_info, array(
-  CURLOPT_URL => $base_url."account/",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: Bearer '.trim($access_token),
-    'Content-Type: application/json'
-  ),
-));
-
-// response
-$response_acc_info = curl_exec($curl_acc_info);
-curl_close($curl_acc_info);
-
-$json_response_acc_info = json_decode($response_acc_info);
-$acc_id =  $json_response_acc_info->{'response'}->{'account_id'};
-// #credit balance in acc
-$cred_bal = $json_response_acc_info->{'response'}->{'credit_balance'};
-
-// #function to check balance
-function checkBal($cred_bal, $num_fee){
-  if ($cred_bal<$num_fee){
-    echo("You do not have enough balance to purchase a number; thus, please add some balance to your account.");
-    return false;
-  } else {
-    return True;
-  }
-}
-
-
-
-// #if function returns true condition will run
-if(checkBal($cred_bal, $num_fee)==True){
-  // #subscribe phone num
-// #phone number into json array
+// Subscribe Phone Num
 $json = array('phnum' => $phnum);
-$json_data = json_encode($json);
+$json = json_encode($json);
 
+//Subscibe
 $curl = curl_init();
-
 curl_setopt_array($curl, array(
-  CURLOPT_URL => $base_url."account/".$acc_id."/phonenumbersubscription",
+  CURLOPT_URL => 'https://public-api.sonetel.com/account/'.$acc_id.'/phonenumbersubscription',
   CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>$json_data,
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: Bearer '.trim($access_token),
-    'Content-Type: application/json'
-  ),
+  CURLOPT_POSTFIELDS =>$json,
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer ' . trim($access_token),
+        'Content-Type: application/json'
+    ),
 ));
 
 $response = curl_exec($curl);
-$response_ouput = json_decode($response);
-$stauts =  $json_response_acc_info->{'status'};
+$response = json_decode($response);
 curl_close($curl);
 
-if ($stauts=="success") {
-  echo("you have subscribed ".$phnum);
-}
-
+//Output
+if ($response->{'status'} == "success") {
+    echo ("$phnum added to your account.");
 }
