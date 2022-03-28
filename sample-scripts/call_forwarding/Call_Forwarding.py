878462f282cc0728a17f318aa5eb1c1d9070ce1b
harshpatel1242 <harshpatel1242@gmail.com>
2022-03-28 15:52:32 +0530

    Update Call_Forwarding.py
    Updated structure

--- sample-scripts/call_forwarding/Call_Forwarding.py
+++ sample-scripts/call_forwarding/Call_Forwarding.py
@@ -1,78 +1,32 @@
 from sre_constants import SUCCESS
 import requests
 import json
-from base64 import b64encode
 
 base_url = "https://public-api.sonetel.com/"
-url_access_token = "https://api.sonetel.com/"
 
-username = input("Enter your username:")
-password = input("Enter the password:")
+#  API Access token
+access_token = "ENTER_ACCESS_TOKEN"
 
-#access token
-access_token = input("Enter the Access Token")
+#forwarde phone number
+fwd_phn_num ="ENTER PHONE NUMBER *DONT USE +*"; #Your phone number in the E164 format without the leading +.
+
+#  Sonetel account ID
+acc_id =  "ENTER_ACCOUNT_ID"
+
+#Choices for user
+# "type 1 to change call forwarding to mobile number""\n"
+# "type 2 to change call forwarding to SIP URL""\n"
+# "type 3 to change call forwarding to voice app""\n"
+choice = 1
 
 #account information  
 headers = {"Authorization": "Bearer {}".format(access_token),
             'Content-Type': 'application/json'
             }
 
-#account info API
-api_acc_info =  requests.get('{}account/'.format(base_url),headers=headers).json()
-acc_id = api_acc_info['response']["account_id"]
-
-#forwarde phone number
-fwd_phn_num =input("Enter phone number") #Your phone number in the E164 format without the leading +.
-
-
-#Choices for user
-# print(#"type 1 to change call forwarding to user""\n"
-#     "type 1 to change call forwarding to mobile number""\n"
-#     "type 2 to change call forwarding to SIP URL""\n"
-#     "type 3 to change call forwarding to voice app""\n")
-choice = int(1) #to mobile number
-
-
-########################payload for forwarding##################
-# if(choice==1):
-#     connect_to_type = "user"
-#     connect_to = input("Enter the user_id")
-#     payload_fwd =json.dumps({
-#   "connect_to_type": connect_to_type,
-#   "connect_to": connect_to
-# })
-
-if(choice==1):
-    connect_to_type = "phnum"
-    connect_to = input("Enter the phone number")
-    payload_fwd =json.dumps({
-      
-  "connect_to_type": connect_to_type,
-  "connect_to": connect_to
-})
-
-elif(choice==2):
-    connect_to_type = "sip"
-    connect_to = input("Enter the sip url")
-    payload_fwd =json.dumps({
-  "connect_to_type": connect_to_type,
-  "connect_to": connect_to
-})
-
-elif(choice==3):
-    response_appid = requests.request("GET", "{}account/{}/voiceapp".format(base_url ,acc_id), headers=headers).json()
-
-    for value in response_appid["response"]:
-        print(value["name"], ":", value["app_id"])
-
-    app_id = input("Enter the ONLY app_id from above list:")
-    connect_to_type = "app"
-    payload_fwd =json.dumps({
-  "connect_to_type": connect_to_type,
-  "connect_to": {
-    "app_type": "ivr",
-    "app_id": app_id
-  }
+payload_fwd = json.dumps({
+  "connect_to_type": "ENTER_TYPE",
+  "connect_to": "ENTER_PARA" 
 })
 
 ########################forwared call####################
