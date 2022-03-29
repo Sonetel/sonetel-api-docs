1b6ef25176980de1f533b75f0c1b2101e5b0519d
harshpatel1242 <harshpatel1242@gmail.com>
2022-03-29 11:21:57 +0530

    Update Subscribe_Phone_Num.py
    Updated Structure

--- sample-scripts/buy-number/Subscribe_Phone_Num.py
+++ sample-scripts/buy-number/Subscribe_Phone_Num.py
@@ -1,54 +1,24 @@
 import requests
 import json
-from base64 import b64encode
-base_url = "https://chat-api.sonetel.com/"
-url_access_token = "https://api.sonetel.com/"
-#inputs
-username = input("Enter your username:")
-password = input("Enter the password:")
-country = input("Enter the name of the country:")
 
-#generate access token
-payload_of_accesstoken={'grant_type': 'password',
-'username': username,
-'password': password,
-'refresh': 'yes'}
-userAndPass = b64encode(b"sonetel-web:sonetel-web").decode("ascii")
-basic_auth_header = { 'Authorization' : 'Basic %s' %  userAndPass }
-gen_acc_token = requests.request("POST", "{}SonetelAuth/beta/oauth/token".format(url_access_token),headers = basic_auth_header, data=payload_of_accesstoken).json()
-access_token = gen_acc_token['access_token']
+base_url = "https://public-api.sonetel.com/"
+
+#  API Access token
+access_token = "ENTER_ACCESS_TOKEN"
+
+#  Phone numumber to purchase
+phnum = "ENTER_PHONE_NUMBER"
+phnum = json.dumps({"phnum": phnum})
+
+# Sonetel account ID
+acc_id =  "ENTER_ACCOUNT_ID"
 
 #account information  
 headers = {"Authorization": "Bearer {}".format(access_token),
             'Content-Type': 'application/json'
             }
-#account info API
-api_acc_info =  requests.get('{}account/'.format(base_url),headers=headers).json()
-acc_id = api_acc_info['response']["account_id"]
 
-# print(api_acc_info)
-cred_bal = api_acc_info['response']['credit_balance']
-# print(cred_bal)
-
-#This will check whether or not the account has a sufficient balance.
-def check_enough_bal():
-
-    #available phone number API
-    avail_numb_country =  requests.get('{}numberstocksummary/{}/availablephonenumber'.format(base_url,country),headers=headers).json()
-    num_fee = avail_numb_country['response'][0]['recurring_fee']
-    global phone_number
-    phone_number = json.dumps({
-                        "phnum": avail_numb_country['response'][0]['phnum'],
-                        })
-    if cred_bal < num_fee:
-        print("You do not have enough balance to purchase a number; thus, please add some balance to your account.")
-        return False
-    else:
-        return True 
-        
-#If the amount is adequate, it will purchase a phone number.
-if(check_enough_bal()):
-
-    #purchase phone number API
-    buy_number = requests.request("POST", '{}account/{}/phonenumbersubscription'.format(base_url,acc_id), headers=headers, data=phone_number).json()
-    print("SUCCESS,you have purchased {}".format(phone_number))
+#purchase phone number API
+response = requests.request("POST", '{}account/{}/phonenumbersubscription'.format(base_url,acc_id), headers=headers, data=phnum).json()
+if response['status']=="success":
+    print("SUCCESS,you have purchased {}".format(phnum))
