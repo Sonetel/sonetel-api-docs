8782ebd51b44bf4d0686c03e4737d0afc8ca11d2
harshpatel1242 <harshpatel1242@gmail.com>
2022-04-21 12:07:53 +0530

    Update Retrive_Recording.php
    minor changes

--- sample-scripts/call-recording/Retrive_Recording.php
+++ sample-scripts/call-recording/Retrive_Recording.php
@@ -3,17 +3,17 @@
 $base_url = "https://public-api.sonetel.com/";
 
 //Enter Date
-$created_date_max = "2022-12-12T12:12:12Z";
+$created_date_max = "yyyy-mm-ddThh:mm:ssZ";
 urlencode($created_date_max);
 
-$created_date_min = "2021-12-12T12:12:12Z";
+$created_date_min = "yyyy-mm-ddThh:mm:ssZ";
 urlencode($created_date_min);
 
 // API Access token
-$access_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhcGkuc29uZXRlbC5jb20iLCJ1c2VyX2lkIjoiMjAxNjU0NzQ5MSIsInVzZXJfbmFtZSI6ImhwdDNAeHBhdGh5Lm5ldCIsInNjb3BlIjpbImFjY291bnQucmVhZCIsImFjY291bnQud3JpdGUiLCJjb252ZXJzYXRpb24ucmVhZCIsImNvbnZlcnNhdGlvbi53cml0ZSIsInVzZXIucmVhZCIsInVzZXIud3JpdGUiXSwiaXNzIjoiU29uZXRlbE5vZGUxMjMiLCJleHAiOjE2NTA2MDY5MzYsImlhdCI6MTY1MDUyMDUzNiwianRpIjoiM2I2ZjgwMGQtMGIyMS00NjhhLWIwZWItNmU2YmM0NDZmNTEwIiwiYWNjX2lkIjoyMDc5MTI0NjUsImNsaWVudF9pZCI6InNvbmV0ZWwtd2ViIn0.cz_X1ZIgU1LP9oxM5_9axRL0Y-nX_U6YkIAcdXzBSjPFTar-pjda7HEWCVE0bf6i3dKqiO9F9EzAlv7NcEPENavOuK9kC-SzGzjV7X25eADRCmcGuwhnJEKJDva9BMWBHeULcwIpGKrHlmlJyvkXISbrMHR0N8eLNqKylTSWS2i4L0Pucfxw1qtoJD51x2Jospheuc8CXGQNdmqDbkKo7jYRmxbVmJ1h7wWvywvAdXlmPO0z0WIA9JybVnrvnr8rH4qNB_1DC25_feNanrpjxqzkikWlH6iYOMETYPThI9NMnVUt2bHcNnIonLhvbhgu4wQ-bWijGyuN6_l4USuNwQ";
+$access_token = "ENTER_ACCESS_TOKEN";
 
 // Sonetel account ID
-$acc_id =  "207943264";
+$acc_id =  "ENTER_ACCOUNT_ID";
 
 // ########################retrive all call recordings##################
 
