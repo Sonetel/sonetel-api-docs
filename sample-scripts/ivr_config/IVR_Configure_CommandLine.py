721e6eee2dd77962f3ec0cf12e3d1430050729fe
Anuj Shivamogga Prakash <115988672+Anuj-Prakash25@users.noreply.github.com>
2023-04-19 12:07:35 +0530

    Update IVR_Configure_CommandLine.py

--- sample-scripts/ivr_config/IVR_Configure_CommandLine.py
+++ sample-scripts/ivr_config/IVR_Configure_CommandLine.py
@@ -4,181 +4,189 @@ import requests
 import os
 
 
-def voiceapp_options(digit, option):
-    voiceapps = {
-        1: [api_req, "play", "prompt", "for  playing a message"],
-        2: [api_req, "call", "user", "for connecting to a user"],
-        3: [api_req, "call", "csg", "for connecting to  a team"],
-        4: [api_req, "call", "phnum", "for connecting to a Phone Number"],
-        5: [api_req, "connect", "app"],
-        6: [api_req, "call", "other"],
-        7: [api_req, "disconnect"]
-    }
-    if option in voiceapps:
-        if option == 7:
-           print("Disconnecting Voiceapp...")
-           return voiceapps[option][0](digit, voiceapps[option][1])
-        elif option == 6:
-             print("Connecting to SIP address...")
-             return voiceapps[option][0](digit, voiceapps[option][1], voiceapps[option][2], option)
-        elif option == 5:
-             ids = input("Choose,\n1 - To select Main Menu\n2 - To select System Prompt\n3 - To select Voice Mail\nEnter your Choice: ")
-             if 1 <= ids <=  3:
-                 return voiceapps[option][0](digit, voiceapps[option][1], voiceapps[option][2], ids)
-             else:
-                  return "Invalid Choice"
-        else:
-            ids = raw_input("Provide the respective id %s: "%voiceapps[option][3])
-            return voiceapps[option][0](digit, voiceapps[option][1], voiceapps[option][2], ids)
-    else:
-        return "Invalid Choice"
+user = os.environ.get('sonetelUserName')
+pwd = os.environ.get('sonetelPassword')
+acc_id = os.environ.get('sonetelAccountId')
+base_url = "https://api.sonetel.com"
 
 
-def digit_options(option):
-    if 0 <= option <= 10:
-        b = input("Choose for updating the voiceapp to,\n1 - Play a Message\n2 - Connect to a User\n3 - Connect to a Team\n4 - Connect to a Phone Number\n5 - Connect to a Voiceapp(like Main Menu, System Prompt or Voice Mail)\n6 - Connect to a SIP Address\n7 - Disconnect the Voiceapp\nEnter your Choice: ")
-        if 1 <= b <= 7:
-            return voiceapp_options(option,b)
-        else:
-            return "Invalid Choice"
-    else:
-        return "Invalid Choice"      
+class VoiceApp:
+
+    def __init__(self, digit, option):
+        self.digit = digit
+        self.option = option
 
 
-def api_req(digit, a, t=None, i=None):
+    def getAccessToken(self):
+        '''This method doesnot accept any arguments but it uses global variables(like user, pwd, acc_id and base_url) to get a new access token every time this method is called. It returns the new access token by adding Bearer as the prefix'''
+        url = base_url + "/SonetelAuth/oauth/token"
 
-    if digit == 10:
-       d = "timeout"
-    else:
-        d = "digit_"+str(digit)
+        print(user, pwd)
+        print(url)
 
-    user = os.environ.get('sonetelUserName')
-    pswd = os.environ.get('sonetelPassword')
-    acc_id = os.environ.get('sonetelAccountId')
+        payload = urllib.urlencode({
+            'grant_type' : 'password',
+            'password' : pwd,
+            'refresh' : 'yes',
+            'username' : user
+        })
 
-    print(user, pswd)
-
-    base_url = "https://api.sonetel.com"
-
-    #--------------GET ACCESS TOKEN------------------
-
-    url = base_url + "/SonetelAuth/oauth/token"
-    print(url)
-
-    payload = urllib.urlencode({
-        'grant_type': 'password',
-        'password': pswd,
-        'refresh':'yes',
-        'username': user
-    })
-
-    headers = {
-        'Accept': 'application/json, text/plain',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    response = requests.request(
-        "POST",
-        url,
-        auth=('sonetel-api', 'sonetel-api'),
-        data=payload,
-        headers=headers
-    )
-
-    response.raise_for_status()
-
-    token = response.json()
-
-    #--------------------GET APP ID----------------------
-
-    at = "Bearer "+ str(token['access_token'])
-    #print(at)
-
-
-    uri1 = base_url + "/account/" + acc_id + "/voiceapp/"
-    print(uri1)
-
-    headers = {
-        'Authorization': at
-    }
-
-    response = requests.request(
-        "GET",
-        uri1,
-        headers = headers
-    )
-
-    response.raise_for_status()
-
-    body = response.json()
-
-    #----------------------UPDATE VOICEAPP----------------------
-
-    #getting default voiceapp id from the API response
-    app_id = None
-    for res in body['response']:
-        if res['app_type'] == "ivr" and res['shortcode'] == "*21":
-            app_id = res['app_id']
-            sip = res['sip_address']
-            break
-
-    print(app_id)
-
-    #getting default voiceapp id and sip address for the choice 5 and 6(connect to a Voiceapp and connect to a SIP Address)
-    if i == 1:
-       i = app_id
-    if i == 2:
-       for res in body['response']:
-          if res['name'] == 'System Prompt':
-             i = res['app_id']
-             break
-    if i == 3:
-       for res in body['response']:
-          if res['name'] == 'Voice Mail':
-             i = res['app_id']
-             break
-    if i == 6:
-       i = sip
-
-    print(i)
-    
-    uri = uri1 + app_id
-    print(uri)
-
-    payload = {
-        "menu": {
-            d: {
-                "action": a,
-                "to": t,
-                "id": i
-            }
+        headers = {
+            'Accept' : 'application/json, text/plain',
+            'Content-Type' : 'application/x-www-form-urlencoded'
         }
-    }
 
-    headers = {
-        'Authorization': at,
-        'Accept': 'application/json, text/plain',
-        'Content-Type': 'application/json'
-    }
-    #print(uri)
-    try:
-        response = requests.put(
-            uri,
-            data=json.dumps(payload),
+        response = requests.request(
+            "POST",
+            url,
+            auth=('sonetel-api', 'sonetel-api'),
+            data=payload,
             headers=headers
+        )
+    
+        response.raise_for_status()
+
+        token = response.json()
+
+        return ("Bearer "+str(token['access_token']))
+
+
+    def getVoiceAppConfig(self, token):
+        '''This method accepts the access token as its argument and through this and the url, it fetches the voiceapp config by making a get request. It returns the url and the response of the api call'''
+        uri = base_url + "/account/" + acc_id + "/voiceapp/"
+        print(uri)
+
+        headers = {
+            'Authorization' : token
+        }
+
+        response = requests.request(
+            "GET",
+            uri,
+            headers = headers
         )
 
         response.raise_for_status()
 
-        body = response.json()
-        if body['status'] == 'failed':
-           print("Error in the api({}) call, here is the body".format(uri))
-           return body
+        return uri, response.json()
+
+    
+    def updateConfig(self):
+        '''This method does not accept any arguments, it compares the user option with the voiceapps dictionary and calls the updateVoiceApp method with the appropriate argument(s)'''
+        voiceapps = {
+            1: ["play", "prompt", "for playing a message"],
+            2: ["call", "user", "for connecting to a user"],
+            3: ["call", "csg", "for connecting to  a team"],
+            4: ["call", "phnum", "for connecting to a Phone Number"],
+            5: ["connect", "app"],
+            6: ["call", "other"],
+            7: ["disconnect"]
+        }
+        if self.option == 7:
+            print("Disconnecting Voiceapp...")
+            return self.updateVoiceApp(voiceapps[self.option][0])
+        elif self.option == 6:
+            print("Connecting to SIP address...")
+            return self.updateVoiceApp(voiceapps[self.option][0], voiceapps[self.option][1], self.option)
+        elif self.option == 5:
+            ids = input("Choose,\n1 - To select Main Menu\n2 - To select System Prompt\n3 - To select Voice Mail\nEnter your Choice: ")
+            if 1 <= ids <=  3:
+                return self.updateVoiceApp(voiceapps[self.option][0], voiceapps[self.option][1], ids)
+            else:
+                return "Invalid Choice"
         else:
-             return body
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+            ids = raw_input("Provide the respective id %s:"%voiceapps[self.option][2])
+            return self.updateVoiceApp(voiceapps[self.option][0], voiceapps[self.option][1], ids)
+        
+        
+    def updateVoiceApp(self, action, to=None, i=None):
+        '''This method accepts 3 arguments in which 1 is mandatory and the other 2 are optional based on the voiceapp update choice. In this method we will call the getAccessToken method and pass it as an argument to the getAppID method call which returns the url and api response from which we will get the appropriate app id. Then we will construct the appropriate payload for put request api call which updates the default voiceapp'''
+        if self.digit == 'timeout':
+            d = self.digit
+        else:
+            d = 'digit_'+str(self.digit)
+
+        t = self.getAccessToken()
+        #print(t)
+        url, body = self.getVoiceAppConfig(t)
+        #print(body)
+
+        for res in body['response']:
+            if res['app_type'] == 'ivr' and res['shortcode'] == '*21':
+                app_id = res['app_id']
+                sip = res['sip_address']
+                break
+
+        print(app_id)
+
+        if i == 1:
+            i = app_id
+        elif i == 2:
+            for res in body['response']:
+                if res['name'] == 'System Prompt':
+                    i = res['app_id']
+                    break
+        elif i == 3:
+            for res in body['response']:
+                if res['name'] == 'Voice Mail':
+                    i = res['app_id']
+                    break
+        elif i == 6:
+            i = sip
+
+        print(i)
+
+        uri = url + app_id
+        print(uri)
+
+        payload = {
+            "menu" : {
+                d : {
+                    "action": action,
+                    "to" : to,
+                    "id" : i
+                }
+            }
+        }
+        #print(payload)
+
+        headers = {
+            'Authorization': t,
+            'Accept': 'application/json, text/plain',
+            'Content-Type': 'application/json'
+        }
+
+        try:
+            response = requests.put(
+                uri,
+                data=json.dumps(payload),
+                headers=headers
+            )
+
+            response.raise_for_status()
+
+            res = response.json()
+            if body['status'] == 'failed':
+                print("Error in the api({}) call, here is the body".format(uri))
+                return res
+            else:
+                return res
+        except requests.exceptions.RequestException as e:
+            print("Error: {}".format(e))
 
 
-c = int(input("Choose the digit of the Voiceapp you want to update (0-10): "))
-print(digit_options(c))
+
+if __name__ == "__main__":
+     i = raw_input("Choose the digit of the Voiceapp you want to update (0-9 or timeout): ")
+     try:
+          if i == 'timeout' or 0 <= int(i) <= 9:
+               b = input("Choose for updating the voiceapp to,\n1 - Play a Message\n2 - Connect to a User\n3 - Connect to a Team\n4 - Connect to a Phone Number\n5 - Connect to a Voiceapp(like Main Menu, System Prompt or Voice Mail)\n6 - Connect to a SIP Address\n7 - Disconnect the Voiceapp\nEnter your Choice: ")
+               if 1 <= b <= 7:
+                    h = VoiceApp(i, b)
+                    print(h.updateConfig())
+               else:
+                    print("Invalid Choice")
+          else:
+               print("Invalid Choice")
+     except Exception:
+          print("Invalid Choice")
